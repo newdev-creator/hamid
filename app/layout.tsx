f7e3e421250de6bb08c8764ad6abdf9d5832ef3b
Julien Freisa <81742220+newdev-creator@users.noreply.github.com>
2024-06-30 18:37:57 +0200

    FAT: tailwind colors + layout components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,18 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Russo_One } from "next/font/google";
+import { Zilla_Slab } from "next/font/google";
 import "./globals.css";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const russoOne = Russo_One({
+  subsets: ["latin"],
+  weight: "400",
+});
+const zillaSlab = Zilla_Slab({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +26,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={zillaSlab.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        white: "#F7F7F7",
+        pink: "#DB2A6B",
+        orange: "#FFB72B",
+        yellow: "#FFE61B",
+        green: "#B5FE83",
+        blue: "#003366",
+        fontBlack: "#0F172A",
       },
     },
   },
