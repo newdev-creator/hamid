5e0c7e7e8fc8e29d18000cb9378a6e39d5cce899
Julien Freisa <81742220+newdev-creator@users.noreply.github.com>
2024-07-25 18:03:09 +0200

    WIP: responsive

--- app/components/EasyStart.tsx
+++ app/components/EasyStart.tsx
@@ -3,6 +3,27 @@ import TitleSection from "./titles/TitleSection";
 import Image from "next/image";
 import TitleCard from "./titles/TitleCard";
 
+const steps = [
+  {
+    id: 1,
+    title: "Create a project",
+    description:
+      "With lots of unique blocks, you can easily build a page without coding.",
+  },
+  {
+    id: 2,
+    title: "Assign related people",
+    description:
+      "With lots of unique blocks, you can easily build a page without coding.",
+  },
+  {
+    id: 3,
+    title: "Make it done on-time",
+    description:
+      "With lots of unique blocks, you can easily build a page without coding.",
+  },
+];
+
 export default function EasyStart() {
   return (
     <>
@@ -19,53 +40,25 @@ export default function EasyStart() {
               <Image
                 src="/assets/img/woman-running.webp"
                 alt="Photo d'une femme qui court sur la plage"
-                // layout="responsive"
+                layout="responsive"
                 width={500}
                 height={300}
               />
             </div>
-            <div className="w-full lg:w-1/2 mt-8 lg:mt-0 lg:ml-12 p-6 rounded-lg">
+
+            <div className="w-full lg:w-1/2 mx-auto mt-8 lg:mt-0 lg:ml-52 p-6 rounded-lg">
               <div className="space-y-8">
-                <div className="flex items-start space-x-4">
-                  <div className="bg-yellow text-fontBlack font-bold rounded-full w-10 h-10 flex items-center justify-center">
-                    1
+                {steps.map((step) => (
+                  <div key={step.id} className="flex items-start space-x-4">
+                    <div className="bg-yellow text-fontBlack font-bold rounded-full w-10 h-10 flex items-center justify-center">
+                      {step.id}
+                    </div>
+                    <div>
+                      <TitleCard titleCard={step.title} />
+                      <p className="text-white w-1/2">{step.description}</p>
+                    </div>
                   </div>
-                  <div>
-                    <TitleCard titleCard="Create a project" />
-                    <p className="text-white w-1/2">
-                      With lots of unique blocks, you can easily build a page
-                      without coding.
-                    </p>
-                  </div>
-                </div>
-                <div className="flex items-start space-x-4">
-                  <div className="bg-yellow-500 text-white rounded-full w-10 h-10 flex items-center justify-center">
-                    2
-                  </div>
-                  <div>
-                    <h3 className="text-xl font-bold text-white">
-                      Assign related people
-                    </h3>
-                    <p className="text-white">
-                      With lots of unique blocks, you can easily build a page
-                      without coding.
-                    </p>
-                  </div>
-                </div>
-                <div className="flex items-start space-x-4">
-                  <div className="bg-yellow-500 text-white rounded-full w-10 h-10 flex items-center justify-center">
-                    3
-                  </div>
-                  <div>
-                    <h3 className="text-xl font-bold text-white">
-                      Make it done on-time
-                    </h3>
-                    <p className="text-white">
-                      With lots of unique blocks, you can easily build a page
-                      without coding.
-                    </p>
-                  </div>
-                </div>
+                ))}
               </div>
             </div>
           </div>
