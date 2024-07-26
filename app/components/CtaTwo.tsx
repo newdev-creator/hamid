9b0ada822b2f3cb3dc5ced586982da3be6cdd6ac
Julien Freisa <81742220+newdev-creator@users.noreply.github.com>
2024-07-26 18:36:31 +0200

    FEAT: CTA2

--- app/components/CtaTwo.tsx
+++ app/components/CtaTwo.tsx
@@ -1,41 +1,37 @@
 import Image from "next/image";
 import React from "react";
+import Button from "./ui/Button";
 
 export default function CtaTwo() {
   return (
-    <div className="bg-blue-900 text-white py-8 px-4 flex justify-center items-center">
+    <div className="bg-fontBlack text-white py-8 px-4 my-32">
       <div className="max-w-6xl mx-auto flex flex-col lg:flex-row items-center">
-        <div className="lg:w-1/4 flex justify-center lg:justify-end mb-4 lg:mb-0">
+        <div className="lg:w-1/4 mb-4 lg:mb-0 relative">
           <Image
             src="/assets/img/CTA/kettelbelt.webp"
             alt="Image 1"
             width={200}
             height={200}
-            className="rounded-lg border-4 border-pink-500"
+            className="rounded-lg border-4 border-white lg:absolute lg:bottom-10"
           />
         </div>
-        <div className="lg:w-1/2 text-center lg:text-left mb-4 lg:mb-0 px-4">
-          <p className="mb-4">
+        <div className="lg:w-1/2 text-center lg:text-left m-10 lg:mb-0 px-4">
+          <p className="text-xl md:text-2xl font-medium mb-10">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis
             lacus ligula. Phasellus pellentesque nisl urna. Aliquam congue nisl
             diam, sed hendrerit libero blandit nec.
           </p>
           <div className="text-center">
-            <a
-              href="#"
-              className="bg-pink-600 text-white px-6 py-3 rounded-full shadow-md hover:bg-pink-700 transition"
-            >
-              Séance découverte 15€
-            </a>
+            <Button href="#" label="Séance découverte 15€" variant="pink" />
           </div>
         </div>
-        <div className="lg:w-1/4 flex justify-center lg:justify-start">
+        <div className="lg:w-1/4 relative">
           <Image
-            src="/assets/img/CTA/girl_training.webp"
+            src="/assets/img/CTA/man_training.webp"
             alt="Image 2"
             width={200}
             height={200}
-            className="rounded-lg border-4 border-white"
+            className="rounded-lg border-4 border-white lg:absolute lg:top-4"
           />
         </div>
       </div>
